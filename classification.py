eba3b174736ed32aad9264c86d72337d8f0d3b9d
Jelena Garic <jeca_997@hotmail.com>
2021-03-04 16:57:41 +0100

    Text preprocessing and labels (tags) vectorization

--- classification.py
+++ classification.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import re
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 path = 'dataset\mpst_full_data.csv'
@@ -7,10 +9,7 @@ def import_dataset(path):
     #return pd.read_csv(path, names=['Title', 'Conditions', 'ONCOLOGY'], skiprows=1)
     return pd.read_csv(path)
 
-if __name__ == '__main__':
-    full_df = import_dataset(path)
-    pd.set_option('max_columns', None)
-    full_df.to_csv('dataset\\dataset.csv')
+def make_data_frames(full_df):
 
     train_df = pd.DataFrame(columns=['imdb_id', 'title','plot_synopsis', 'tags'])
     test_df = pd.DataFrame(columns=['imdb_id', 'title','plot_synopsis', 'tags'])
@@ -42,3 +41,73 @@ if __name__ == '__main__':
     train_df.to_csv('dataset\\train_dataset.csv')
     test_df.to_csv('dataset\\test_dataset.csv')
     val_df.to_csv('dataset\\validation_dataset.csv')
+    return train_df, test_df, val_df
+
+def decontracted(phrase):
+    # specific
+    phrase = re.sub(r"won't", "will not", phrase)
+    phrase = re.sub(r"can\'t", "can not", phrase)
+
+    # general
+    phrase = re.sub(r"n\'t", " not", phrase)
+    phrase = re.sub(r"\'re", " are", phrase)
+    phrase = re.sub(r"\'s", " is", phrase)
+    phrase = re.sub(r"\'d", " would", phrase)
+    phrase = re.sub(r"\'ll", " will", phrase)
+    phrase = re.sub(r"\'t", " not", phrase)
+    phrase = re.sub(r"\'ve", " have", phrase)
+    phrase = re.sub(r"\'m", " am", phrase)
+    return phrase
+stopwords= set(['br', 'the', 'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've",\
+            "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', \
+            'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their',\
+            'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', \
+            'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', \
+            'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', \
+            'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after',\
+            'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further',\
+            'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',\
+            'most', 'other', 'some', 'such', 'only', 'own', 'same', 'so', 'than', 'too', 'very', \
+            's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', \
+            've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn',\
+            "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn',\
+            "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", \
+            'won', "won't", 'wouldn', "wouldn't"])
+
+df = import_dataset(path)
+pd.set_option('max_columns', None)
+
+preprocessed_synopsis = []
+
+for sentence in df['plot_synopsis'].values:
+    sentence = decontracted(sentence)
+    sentence = re.sub("\S*\d\S*", "", sentence).strip()
+    sentence = re.sub('[^A-Za-z]+', ' ', sentence)
+    sentence = ' '.join(e.lower() for e in sentence.split() if e.lower() not in stopwords)
+    preprocessed_synopsis.append(sentence.strip())
+
+df['preprocessed_plots']=preprocessed_synopsis
+df.to_csv('dataset\\dataset.csv')
+
+def remove_spaces(x):
+    x=x.split(",")
+    nospace=[]
+    for item in x:
+        item=item.lstrip()
+        nospace.append(item)
+    return (",").join(nospace)
+
+df['tags']=df['tags'].apply(remove_spaces)
+
+train_df=df.loc[df.split=='train']
+train_df=train_df.reset_index()
+test_df=df.loc[df.split=='test']
+test_df=test_df.reset_index()
+val_df=df.loc[df.split=="val"]
+val_df=val_df.reset_index()
+
+vectorizer = CountVectorizer(tokenizer = lambda x: x.split(","), binary='true')
+y_train = vectorizer.fit_transform(train_df['tags']).toarray()
+y_test = vectorizer.transform(test_df['tags']).toarray()
+
+print(y_train)
